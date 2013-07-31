1bb435b332ff66a74fbc359ad0715e7d3f8da9e6
Stefan Urbanek <stefan@agentfarms.net>
2013-07-30 22:06:55 -0400

    added example

---
+++ examples/allowing_compiler.py
@@ -0,0 +1,32 @@
+from expressions import Expression, ExpressionError
+
+class SecureCompiler(object):
+    def __init__(self, allow=None):
+        self.allow = allow or []
+    def compile_literal(self, literal):
+        return repr(literal)
+    def compile_variable(self, variable):
+        if self.allow and variable not in self.allow:
+            raise ExpressionError("Variable %s is not allowed" % variable)
+
+        return variable
+
+    def compile_operator(self, operator, op1, op2):
+        return "(%s %s %s)" % (op1, operator, op2)
+    def compile_function(self, function, args):
+        arglist = ", " % args
+        return "%s(%s)" % (function, arglist)
+
+compiler = SecureCompiler(allow=["a", "b"])
+
+expr = Expression("a + b")
+result = expr.compile(compiler)
+
+a = 1
+b = 1
+print(eval(result))
+
+
+expr = Expression("a + c")
+result = expr.compile(compiler)
+print(eval(result))
