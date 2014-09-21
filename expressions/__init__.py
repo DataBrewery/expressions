b4cec2888d33f150aaa652a789b7f1f6fbf1015d
Stefan Urbanek <stefan@agentfarms.net>
2014-09-20 21:04:10 -0400

    v0.2.1

--- expressions/__init__.py
+++ expressions/__init__.py
@@ -1,4 +1,4 @@
 from .compiler import *
 
-__version__ = '0.2'
+__version__ = '0.2.1'
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "expressions",
-    version = "0.2",
+    version = "0.2.1",
 
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data = {
