a42cef761d2ced99a241714b82c70a0060a922c7
Stefan Urbanek <stefan@agentfarms.net>
2014-09-20 21:03:12 -0400

    Added Python 3 and Python 2 compatibility module

---
+++ expressions/compat.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+"""Pytho compatibility utilities"""
+
+from __future__ import absolute_import
+
+import sys
+
+py3k = sys.version_info >= (3, 0)
+
+if py3k:
+    string_type = str
+    text_type = str
+
+    def unicode_escape(s):
+        return bytes(s, "utf-8").decode("unicode_escape")
+
+else:
+    string_type = basestring
+    text_type = unicode
+
+    def unicode_escape(s):
+         return s.decode("string-escape")
+
