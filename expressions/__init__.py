ea47b9b463a7e0bc2393166843d3dccb171b99ee
Stefan Urbanek <stefan@agentfarms.net>
2015-03-26 23:22:49 -0400

    Bump version to 0.2.2 (for Cubes expr.)

--- expressions/__init__.py
+++ expressions/__init__.py
@@ -1,4 +1,4 @@
 from .compiler import *
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "expressions",
-    version = "0.2.1",
+    version = "0.2.2",
 
     packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data = {
